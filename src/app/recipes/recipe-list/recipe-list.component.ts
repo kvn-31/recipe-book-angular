17cf4636b16da08d37a1ce485508b3dc466bcec4
kWinCode <kevin.pirner@live.at>
2021-04-19 18:13:12 +0200

    added ingredient model, adapted various view including edit and detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../shared/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Vegane Bolognese', 'Simply the best', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/vegan-bolognese-b4f9b85.jpg'),
+    new Recipe('Chicken Micken', 'Simply cruel', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/vegan-bolognese-b4f9b85.jpg')
+
+  ];
 
   constructor() { }
 
---
+++ src/app/shared/models/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+    // shortcut
+    constructor(public name: string, public amount: number) {}
+}
