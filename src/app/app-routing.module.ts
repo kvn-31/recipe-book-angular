09d17c194a7b444c9fca7994f0bc085fe4d3fe29
kWinCode <kevin.pirner@live.at>
2021-06-07 10:59:06 +0200

    added lazy loading for all feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,23 @@ const routes: Routes = [
 		redirectTo: 'recipes',
 		pathMatch: 'full',
 	},
+	{
+		path: 'recipes',
+		loadChildren: () =>
+			import('./recipes/recipes.module').then((m) => m.RecipesModule),
+	},
+	{
+		path: 'authenticate',
+		loadChildren: () =>
+			import('./auth/auth.module').then((m) => m.AuthModule),
+	},
+	{
+		path: 'shopping-list',
+		loadChildren: () =>
+			import('./shopping-list/shopping-list.module').then(
+				(m) => m.ShoppingListModule
+			),
+	},
 ]
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { SharedModule } from '../shared/shared.module'
 import { AuthComponent } from './auth.component'
 const routes: Routes = [
 	{
-		path: 'authenticate',
+		path: '',
 		component: AuthComponent,
 	},
 ]
