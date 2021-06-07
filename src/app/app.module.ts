b338d855850a4c4b8a7cab52958def759213e958
kWinCode <kevin.pirner@live.at>
2021-06-07 09:59:51 +0200

    set up feature module for shopping list and recipe, set up core module for services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,18 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
-import { ShoppingListComponent } from './shopping-list/shopping-list.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { DropdownDirective } from './shared/dropdown.directive'
-import { RecipeService } from './shared/services/recipe.service'
-import { ShoppingListService } from './shared/services/shopping-list.service'
 import { AuthComponent } from './auth/auth.component'
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component'
-import { AuthInterceptorService } from './shared/services/auth-interceptor.service'
-import { AlertComponent } from './shared/alert/alert.component'
-import { PlaceholderDirective } from './shared/placeholder-directive/placeholder.directive'
 import { RecipesModule } from './recipes/recipes.module'
+import { ShoppingListModule } from './shopping-list/shopping-list.module'
+import { SharedModule } from './shared/shared.module'
+import { CoreModule } from './core.module'
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		ShoppingEditComponent,
-		ShoppingListComponent,
-		DropdownDirective,
-		AuthComponent,
-		LoadingSpinnerComponent,
-		AlertComponent,
-		PlaceholderDirective,
-	],
+	declarations: [AppComponent, HeaderComponent, AuthComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
@@ -36,16 +20,11 @@ import { RecipesModule } from './recipes/recipes.module'
 		ReactiveFormsModule,
 		HttpClientModule,
 		RecipesModule,
+		ShoppingListModule,
+		SharedModule,
+		CoreModule,
 	],
-	providers: [
-		ShoppingListService,
-		RecipeService,
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: AuthInterceptorService,
-			multi: true,
-		},
-	],
+	providers: [],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
