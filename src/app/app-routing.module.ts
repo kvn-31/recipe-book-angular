80ba32f0e8a9928ab53a9b855713d84bbfe12c1b
kWinCode <kevin.pirner@live.at>
2021-06-07 10:07:17 +0200

    added auth module incl routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { AuthComponent } from './auth/auth.component'
 
 const routes: Routes = [
 	{
 		path: '',
 		redirectTo: 'recipes',
 		pathMatch: 'full',
-	},
-	{
-		path: 'authenticate',
-		component: AuthComponent,
 	},
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,22 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { AuthComponent } from './auth/auth.component'
 import { RecipesModule } from './recipes/recipes.module'
 import { ShoppingListModule } from './shopping-list/shopping-list.module'
 import { SharedModule } from './shared/shared.module'
 import { CoreModule } from './core.module'
+import { AuthModule } from './auth/auth.module'
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, AuthComponent],
+	declarations: [AppComponent, HeaderComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		FormsModule,
-		ReactiveFormsModule,
 		HttpClientModule,
 		RecipesModule,
 		ShoppingListModule,
 		SharedModule,
 		CoreModule,
+		AuthModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
